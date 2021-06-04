406c6453b87e4983d765bb34605a50f17d04ac1a
Crystal <44468822+cyl940709@users.noreply.github.com>
2021-06-05 02:35:47 +0800

    Delete merge.py

--- merge.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-class Solution:
-    def merge(self, nums1, m: int, nums2, n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        nums1[:] = sorted(nums1[:m] + nums2)
